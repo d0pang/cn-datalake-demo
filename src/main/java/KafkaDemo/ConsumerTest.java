de7a339435dc583a26263b800555495682bb8c7f
Dylan Pang <Dylan.Pang@walmart.com>
2019-06-27 16:18:59 +0800

    upate consumer method.

--- src/main/java/KafkaDemo/ConsumerTest.java
+++ src/main/java/KafkaDemo/ConsumerTest.java
@@ -5,11 +5,13 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Properties;
 
 /**
@@ -37,37 +39,41 @@ public class ConsumerTest {
         System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
 //        System.setProperty("sun.security.krb5.debug", "true");
 
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerlist);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "DemoConsumer");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put("security.protocol", "SASL_PLAINTEXT");
-        props.put("sasl.kerberos.service.name", "kafka");
+        Properties consumerConfig = new Properties();
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerlist);
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "DemoConsumer2");
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        consumerConfig.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        consumerConfig.put("security.protocol", "SASL_PLAINTEXT");
+        consumerConfig.put("sasl.kerberos.service.name", "kafka");
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-
-        TopicPartition partition0 = new TopicPartition(topic_name, 0);
-        TopicPartition partition1 = new TopicPartition(topic_name, 1);
-        TopicPartition partition2 = new TopicPartition(topic_name, 2);
-
-        consumer.assign(Arrays.asList(partition0, partition1, partition2));
-
-        ConsumerRecords<String, String> records = null;
+        KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(consumerConfig);
+        TestConsumerRebalanceListener rebalanceListener = new TestConsumerRebalanceListener();
+        consumer.subscribe(Collections.singletonList(topic_name), rebalanceListener);
 
         while (true) {
-            try {
-                Thread.sleep(10000l);
-                System.out.println();
-                records = consumer.poll(Long.MAX_VALUE);
-                for (ConsumerRecord<String, String> record : records) {
-                    System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
+            for (ConsumerRecord<byte[], byte[]> record : records) {
+                System.out.printf("Received Message topic =%s, partition =%s, offset = %d, key = %s, value = %s\n", record.topic(), record.partition(), record.offset(), record.key(), record.value());
             }
+
+            consumer.commitSync();
         }
     }
+
+    private static class  TestConsumerRebalanceListener implements ConsumerRebalanceListener {
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            System.out.println("Called onPartitionsRevoked with partitions:" + partitions);
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            System.out.println("Called onPartitionsAssigned with partitions:" + partitions);
+        }
+    }
+
 }
